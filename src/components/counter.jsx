70b708d910a83be6332981a38d3b5204f5fd940e
Kandy-Hamisi <hkandy2000@gmail.com>
2021-07-15 09:18:22 +0300

    passing children dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0,
+        value: this.props.value,
         // imageUrl: 'https://picsum.photos/200'
         tags: ['tag1', 'tag2', 'tag3']
     };
@@ -26,12 +26,12 @@ class Counter extends Component {
     }
 
     //changing this function into an arrow function solves the problem of accessing this
-    handleIncrement = product =>{
+    handleIncrement = () =>{
         // console.log("increment Clicked", this);
 
         // this.state.count++;
-        console.log(product);
-        this.setState({count: this.state.count + 1});
+        // console.log(product);
+        this.setState({value: this.state.value + 1});
     }
 
     //  doHandleIncrement = () =>{
@@ -39,15 +39,18 @@ class Counter extends Component {
     //  }
 
     render() { 
+        
+
+        // console.log("props", this.props);
 
         return (
             <div>
-            
+                {/* <h4>{this.props.id}</h4> */}
                 <span className={ this.getBadgeClasses() }>
                     {this.formatCount()}
                 </span>
                 <button
-                    onClick={() => this.handleIncrement({id: this.formatCount()})}
+                    onClick={() => this.handleIncrement()}
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
@@ -65,12 +68,12 @@ class Counter extends Component {
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         // if the count is 'Zero' then give the badge a warning class else a primary class
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
